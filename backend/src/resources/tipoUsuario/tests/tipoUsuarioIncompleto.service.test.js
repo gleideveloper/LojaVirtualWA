a00ef9e0ec807187ab58335fb1f19a29694c1e54
Gleides Vinente <gleidevelop@gmail.com>
2023-08-04 20:48:22 -0400

    add testes tipoUsuarioIncompleto

--- backend/src/resources/tipoUsuario/tests/tipoUsuarioIncompleto.service.test.js
+++ backend/src/resources/tipoUsuario/tests/tipoUsuarioIncompleto.service.test.js
@@ -1,19 +1,30 @@
 import request from 'supertest';
 import { server } from '../../../index';
 import connection from '../../../db/config';
+import {describe} from "node:test";
 
 describe('tipoUsuario Service', () => {
-  beforeAll(async () => {
-    await server.bootstrap();
-  });
+    // Limpar o banco de dados e inserir dados de teste antes de executar os testes
+    beforeAll(async () => {
+        await server.bootstrap();
+    });
+    /**  implementar - 2,5 **/
+    // Teste para verificar se a rota GET /tipo-usuario está retornando todos os tipos de usuário
+    it('should get all user types', async () => {
+        const res = await request(server.server).get('/v1/tipo-usuario');
 
-  /**  implementar - 2,5
-  it('should get all user types', async () => {
-   
-  });
-  */
+        expect(res.statusCode).toEqual(200);
+        expect(Array.isArray(res.body)).toBeTruthy();
+        expect(res.body.length).toBeGreaterThan(0);
 
-  afterAll(async () => {
-    await connection.close();
-  });
+        res.body.forEach((tipoUsuario: any) => {
+            expect(tipoUsuario).toHaveProperty('id');
+            expect(tipoUsuario).toHaveProperty('rotulo');
+        });
+    });
+
+    // Fechar a conexão com o banco de dados após os testes
+    afterAll(async () => {
+        await connection.close();
+    });
 });
